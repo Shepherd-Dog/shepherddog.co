cd394a5d6890846ce9fffc95447928bbe56b2cea
David Brunow <helloDavid@brunow.org>
2015-04-23 14:49:36 -0500

    Animate menu on mobile so users know they can scroll

--- scripts/shepherd-dog.js
+++ scripts/shepherd-dog.js
@@ -14,7 +14,46 @@ var ShepherdDog = function ()
 
 		$(".menu-button-container").find("a").on("click", function ()
 		{
-			$(".main-nav-list-container").slideToggle();
+			//$(".main-nav-list-container").slideToggle();
+			$(".main-nav-list-container").css({ "z-index": -1 });
+
+			if($(".main-nav-list-container").css("visibility") === "visible")
+			{
+				$(".main-nav-list-container").animate({ "height": "0" }, 300, function () {
+					$(".main-nav-list-container").css({ visibility: "hidden" });
+				});
+
+			}
+			else
+			{
+				$(".main-nav-list-container").animate({ scrollLeft: "150px" }, 0);
+
+				$(".main-nav-list-container").css({ visibility: "visible" });
+
+				$(".main-nav-list-container").animate({ "height": "60px" }, 300);
+
+				if(window.location.href.toLowerCase().indexOf("pricing") !== -1)
+				{
+					$(".main-nav-list-container").animate({ scrollLeft: "125px" }, 300);
+				}
+				else if(window.location.href.toLowerCase().indexOf("who-we-are") !== -1)
+				{
+					$(".main-nav-list-container").animate({ scrollLeft: "300px" }, 300);
+				}
+				else if(window.location.href.toLowerCase().indexOf("our-work") !== -1)
+				{
+					$(".main-nav-list-container").animate({ scrollLeft: "300px" }, 300);
+				}
+				else if(window.location.href.toLowerCase().indexOf("blog") !== -1)
+				{
+					$(".main-nav-list-container").animate({ scrollLeft: "300px" }, 300);
+				}
+				else
+				{
+					$(".main-nav-list-container").animate({ scrollLeft: "0" }, 300);	
+				}
+			}
+
 		});
 	};
 };
